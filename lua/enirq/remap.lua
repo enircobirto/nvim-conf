08513c6c282d7b8f703ffec863dde594054439e9
Henirque Birto <henriquezbrt@gmail.com>
2024-01-09 15:54:03 -0300

    Remap ' to `

--- lua/enirq/remap.lua
+++ lua/enirq/remap.lua
@@ -56,3 +56,5 @@ vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
 vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
 vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+
+vim.keymap.set("n", "'", "`", opts)
